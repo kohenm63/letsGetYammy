e304d1bfaa4bf9f5f8aec78470bcf50308a02bd1
mkohen <email>
2020-02-13 16:31:07 +0200

    Updates

---
+++ precache-manifest.6d7da4964c1f2a559fe23f57d0eec22c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcbd8bde301529512269efe5972decd",
+    "url": "/letsGetYammy/index.html"
+  },
+  {
+    "revision": "9f89ab49579c8510b9f8",
+    "url": "/letsGetYammy/static/css/main.0a7bc87d.chunk.css"
+  },
+  {
+    "revision": "c63e7063f4476298f03d",
+    "url": "/letsGetYammy/static/js/2.2d004f31.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/letsGetYammy/static/js/2.2d004f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f89ab49579c8510b9f8",
+    "url": "/letsGetYammy/static/js/main.91a76623.chunk.js"
+  },
+  {
+    "revision": "431b052e0ea48bf2ef82",
+    "url": "/letsGetYammy/static/js/runtime-main.98662c65.js"
+  }
+]);
